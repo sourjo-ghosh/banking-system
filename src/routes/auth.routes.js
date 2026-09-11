14adc068a852b56e21f731cc3461e7f6b7f21f33
sourjo-ghosh <joyghosh211@outlook.com>
2026-09-11 14:36:11 +0600

    add user login functionality with JWT authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const  router = express.Router();
 const authController = require("../controllers/auth.controller")
 
+
+/**
+ * POST /api/auth/register
+*/
 router.post("/register", authController.userRegisterController);
+
+/**
+ * POST /api/auth/login
+ */
+router.post("/login", authController.userLoginController);
 
 module.exports = router;
